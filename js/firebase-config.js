7aeec730d55faff83cfc02877786553abdc7dab0
Gw team <codewithprashant07@gmail.com>
2026-06-20 14:10:46 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDSSUn42qoB7ZcrPeJ9sZIImsx0d-Z6TXc",
+  authDomain: "fixdarbhanga.firebaseapp.com",
+  projectId: "fixdarbhanga",
+  storageBucket: "fixdarbhanga.firebasestorage.app",
+  messagingSenderId: "104519114133",
+  appId: "1:104519114133:web:d03d69d6fdadb3613faa9c"
 };
 
 const app = initializeApp(firebaseConfig);
